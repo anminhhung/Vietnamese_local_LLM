ec90083a996a365d2cb0705fdf72f41f990ec978
BachNgoH <nlmbao2015@gmail.com>
2024-01-16 09:33:31 +0700

    update streamlit

--- src/data_generation/generate_response.py
+++ src/data_generation/generate_response.py
@@ -7,18 +7,20 @@ prompt_template = """
 """
 
 
+prompt = "Attention là gì?"
+
 def main():
-    llm = LLM(model="vilm/vinallama-7b-chat",  trust_remote_code=True, max_model_len=1024)
+    llm = LLM(model="bkai-foundation-models/vietnamese-llama2-7b-120GB",  trust_remote_code=True, max_model_len=1024)
     sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=1024)
 
     # load instruction from file
-    with open("/home/server1-ailab/Desktop/Bach/Vietnamese_local_LLM/data/augment_ques.json", "r") as f:
-        prompts = json.load(f)
+    # with open("/home/server1-ailab/Desktop/Bach/Vietnamese_local_LLM/data/augment_ques.json", "r") as f:
+    #     prompts = json.load(f)
     
-    for prompt in prompts[:10]:
-        outputs =  llm.generate(prompts=prompt_template.format(question=prompt.replace("7.", "").replace("\n", "")), sampling_params=sampling_params)
-        for output in outputs:
-            print(output.prompt)
-            print( output.outputs[0].text )
+    # for prompt in prompts[:10]:
+    outputs =  llm.generate(prompts=prompt_template.format(question=prompt.replace("7.", "").replace("\n", "")), sampling_params=sampling_params)
+    for output in outputs:
+        print(output.prompt)
+        print( output.outputs[0].text )
 if __name__ == "__main__":
     main()
--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -18,6 +18,7 @@ from langchain.schema.retriever import BaseRetriever, Document
 from langchain.callbacks.manager import CallbackManagerForRetrieverRun
 from langchain.cache import SQLiteCache
 from langchain.llms.vllm import VLLM
+from run_localGPT import load_model, retrieval_qa_pipline
 
 from typing import List 
 
@@ -53,149 +54,14 @@ from src.constants import (
 import streamlit as st
 from accelerate import Accelerator
 
-class DummyRetriever(BaseRetriever):
-    def _get_relevant_documents(
-        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
-    ) -> List[Document]:
-        return []
-
 @st.cache_resource()
 def load_model(device_type="cuda", model_id="", model_basename=None, LOGGING=logging):
-    """
-    Select a model for text generation using the HuggingFace library.
-    If you are running this for the first time, it will download a model for you.
-    subsequent runs will use the model from the disk.
-
-    Args:
-        device_type (str): Type of device to use, e.g., "cuda" for GPU or "cpu" for CPU.
-        model_id (str): Identifier of the model to load from HuggingFace's model hub.
-        model_basename (str, optional): Basename of the model if using quantized models.
-            Defaults to None.
-
-    Returns:
-        HuggingFacePipeline: A pipeline object for text generation using the loaded model.
-
-    Raises:
-        ValueError: If an unsupported model or device type is provided.
-    """
-    logging.info(f"Loading Model: {model_id}, on: {device_type}")
-    logging.info("This action can take a few minutes!")
-
-    if model_basename is not None:
-        if ".gguf" in model_basename.lower():
-            print("Load quantized model gguf")
-            llm = load_quantized_model_gguf_ggml(model_id, model_basename, device_type, LOGGING)
-            return llm
-        elif ".ggml" in model_basename.lower():
-            print("Load quantized model ggml")
-            model, tokenizer = load_quantized_model_gguf_ggml(model_id, model_basename, device_type, LOGGING)
-        elif ".awq" in model_basename.lower():
-            print("Load quantized model awq")
-            model, tokenizer = load_quantized_model_awq(model_id, LOGGING)
-        else:
-            print("Load quantized model qptq")
-            model, tokenizer = load_quantized_model_qptq(model_id, model_basename, device_type, LOGGING)
-    else:
-        print("load_full_model")
-        # model, tokenizer = load_full_model(model_id, model_basename, device_type, LOGGING)
-        llm = VLLM(model=model_id, trust_remote_code=True, max_new_tokens=MAX_NEW_TOKENS, temperature=0.7, top_k=10, top_p=0.95)
-        return llm
-
-    # Load configuration from the model to avoid warnings
-    generation_config = GenerationConfig.from_pretrained(model_id)
-    # see here for details:
-    # https://huggingface.co/docs/transformers/
-    # main_classes/text_generation#transformers.GenerationConfig.from_pretrained.returns
-
-    # Create a pipeline for text generation
-    pipe = pipeline(
-        "text-generation",
-        model=model,
-        tokenizer=tokenizer,
-        max_length=MAX_NEW_TOKENS,
-        temperature=0.2,
-        # top_p=0.95,
-        repetition_penalty=1.15,
-        generation_config=generation_config,
-    )
-
-    local_llm = HuggingFacePipeline(pipeline=pipe)
-    logging.info("Local LLM Loaded")
-
-    return local_llm
+    return load_model(device_type, model_id=model_id, model_basename=model_basename, LOGGING=LOGGING)
 
 
 @st.cache_resource()
 def retrieval_qa_pipline(device_type="cpu", use_history=False, promptTemplate_type="llama", use_retriever=True):
-    """
-    Initializes and returns a retrieval-based Question Answering (QA) pipeline.
-
-    This function sets up a QA system that retrieves relevant information using embeddings
-    from the HuggingFace library. It then answers questions based on the retrieved information.
-
-    Parameters:
-    - device_type (str): Specifies the type of device where the model will run, e.g., 'cpu', 'cuda', etc.
-    - use_history (bool): Flag to determine whether to use chat history or not.
-
-    Returns:
-    - RetrievalQA: An initialized retrieval-based QA system.
-
-    Notes:
-    - The function uses embeddings from the HuggingFace library, either instruction-based or regular.
-    - The Chroma class is used to load a vector store containing pre-computed embeddings.
-    - The retriever fetches relevant documents or data based on a query.
-    - The prompt and memory, obtained from the `get_prompt_template` function, might be used in the QA system.
-    - The model is loaded onto the specified device using its ID and basename.
-    - The QA system retrieves relevant documents using the retriever and then answers questions based on those documents.
-    """
-
-    embeddings = HuggingFaceInstructEmbeddings(model_name=EMBEDDING_MODEL_NAME, model_kwargs={"device": device_type})
-    # uncomment the following line if you used HuggingFaceEmbeddings in the ingest.py
-    # embeddings = HuggingFaceEmbeddings(model_name=EMBEDDING_MODEL_NAME)
-
-    # load the vectorstore
-    db = Chroma(
-        persist_directory=PERSIST_DIRECTORY,
-        embedding_function=embeddings,
-        client_settings=CHROMA_SETTINGS
-    )
-    retriever = db.as_retriever()
-    embeddings_filter = EmbeddingsFilter(embeddings=embeddings, similarity_threshold=cfg.MODEL.SIMILARITY_THRESHOLD)
-    retriever = ContextualCompressionRetriever(
-        base_compressor=embeddings_filter, base_retriever=retriever
-    )
-
-    if not use_retriever:
-        retriever = DummyRetriever()
-
-    # get the prompt template and memory if set by the user.
-    prompt, memory = get_prompt_template(promptTemplate_type=promptTemplate_type, history=use_history)
-
-    # load the llm pipeline
-    llm = load_model(device_type, model_id=MODEL_ID, model_basename=MODEL_BASENAME, LOGGING=logging)
-
-    if use_history:
-        qa = RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type="stuff",  # try other chains types as well. refine, map_reduce, map_rerank
-            retriever=retriever,
-            return_source_documents=True,  # verbose=True,
-            callbacks=callback_manager,
-            chain_type_kwargs={"prompt": prompt, "memory": memory},
-        )
-    else:
-        qa = RetrievalQA.from_chain_type(
-            llm=llm,
-            chain_type="stuff",  # try other chains types as well. refine, map_reduce, map_rerank
-            retriever=retriever,
-            return_source_documents=True,  # verbose=True,
-            callbacks=callback_manager,
-            chain_type_kwargs={
-                "prompt": prompt,
-            },
-        )
-
-    return qa
+    return retrieval_qa_pipline(device_type, use_history=use_history, promptTemplate_type=promptTemplate_type, use_retriever=use_retriever) 
 
 def run_app(qa_pipeline):    
     st.title("💬 Chatbot")
