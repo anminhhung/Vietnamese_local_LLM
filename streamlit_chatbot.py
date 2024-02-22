05ce024b41d6fb7b805711bfc7acfe5174562d5d
BachNgoH <nlmbao2015@gmail.com>
2024-02-22 14:49:29 +0700

    Streamlit authentication

--- streamlit_chatbot.py
+++ streamlit_chatbot.py
@@ -1,70 +1,96 @@
+import pickle
+from pathlib import Path
+import streamlit as st 
+import streamlit_authenticator as stauth 
 import os
 import logging
 import requests
 import ray
-import streamlit as st
 from src import utils
-
 
 def send_query(text):
     resp = requests.post("http://localhost:8000/api/stream?query={}".format(text), stream=True)
 
     return resp
 
-
 def run_app():    
+    # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+    st.set_page_config(page_title="localbot", page_icon="🧑‍💼", layout="wide")
+
     st.title("💬 Chatbot")
     st.caption("🚀 I'm a Local Bot")
     
-    # show source use to debug
-    # show_sources = st.checkbox("Show sources", value=False)
+    # --- USER AUTHENTICATION ---
+    names = ["hungam"]
+    usernames = ["hungam"]
 
-    if "messages" not in st.session_state:
-        st.session_state.messages = [{"role": "assistant", "content": "Tôi có thể giúp gì được cho bạn?"}]
+    # load hashed passwords
+    file_path = Path(__file__).parent / "hashed_pw.pkl"
+    with file_path.open("rb") as file:
+        hashed_passwords = pickle.load(file)
 
-    for msg in st.session_state.messages:
-        st.chat_message(msg["role"]).write(msg["content"])
-        
-    # Initialize the QA system using caching
-    # translater = Translation(from_lang="en", to_lang='vi', mode='translate') 
-    if query := st.chat_input():
-        st.session_state.messages.append({"role": "user", "content": query})
-        st.chat_message("user").write(query)
-        
-        # Add spinner
-        with st.spinner("Thinking..."):
-            res = send_query(query)
-            res.raise_for_status()
-            # translate
-            # res = translator.translate(res, dest="vi").text
-            answer = res
-            with st.chat_message("assistant"):
-                message_placeholder = st.empty()
-                full_response = ""
-            for chunk in res.iter_content(chunk_size=None, decode_unicode=True):
-                full_response += chunk
-                message_placeholder.markdown(full_response + "▌")
+    authenticator = stauth.Authenticate(names, usernames, hashed_passwords, 
+                                        "sales_dashboard", "abcdef", cookie_expiry_days=30)
+
+    name, authentication_status, username = authenticator.login("Login", "main")
+
+    if authentication_status == False:
+        st.error("Username/password is incorrect")
+
+    if authentication_status == None:
+        st.warning("Please enter your username and password")
+
+    if authentication_status:
+        # ---- SIDEBAR ----
+        authenticator.logout("Logout", "sidebar")
+        st.sidebar.title(f"Welcome {name}")
+
+        if "messages" not in st.session_state:
+            st.session_state.messages = [{"role": "assistant", "content": "Tôi có thể giúp gì được cho bạn?"}]
+
+        for msg in st.session_state.messages:
+            st.chat_message(msg["role"]).write(msg["content"])
             
-            message_placeholder.markdown(full_response)
+        # Initialize the QA system using caching
+        # translater = Translation(from_lang="en", to_lang='vi', mode='translate') 
+        if query := st.chat_input():
+            st.session_state.messages.append({"role": "user", "content": query})
+            st.chat_message("user").write(query)
+            
+            # Add spinner
+            with st.spinner("Thinking..."):
+                res = send_query(query)
+                res.raise_for_status()
+                # translate
+                # res = translator.translate(res, dest="vi").text
+                answer = res
+                with st.chat_message("assistant"):
+                    message_placeholder = st.empty()
+                    full_response = ""
+                for chunk in res.iter_content(chunk_size=None, decode_unicode=True):
+                    full_response += chunk
+                    message_placeholder.markdown(full_response + "▌")
+                
+                message_placeholder.markdown(full_response)
 
-        # if translate_output:
-        #     answer = translater(answer)
-        
-        st.session_state.messages.append({"role": "assistant", "content": full_response})
-        # st.chat_message("assistant").write(answer)
-        
-        # response = answer
-        
-        # if show_sources:
-        #     response += "\n\n"
-        #     response += "----------------------------------SOURCE DOCUMENTS---------------------------\n"
-        #     for document in docs:
-        #         response += "\n> " + document.metadata["source"] + ":\n" + document.page_content
-        #     response += "----------------------------------SOURCE DOCUMENTS---------------------------\n"
-        
-        # save_qa
-        utils.log_to_csv(query, answer)
-        st.chat_message("assistant").write(answer)
+            # if translate_output:
+            #     answer = translater(answer)
+            
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            # st.chat_message("assistant").write(answer)
+            
+            # response = answer
+            
+            # if show_sources:
+            #     response += "\n\n"
+            #     response += "----------------------------------SOURCE DOCUMENTS---------------------------\n"
+            #     for document in docs:
+            #         response += "\n> " + document.metadata["source"] + ":\n" + document.page_content
+            #     response += "----------------------------------SOURCE DOCUMENTS---------------------------\n"
+            
+            # save_qa
+            utils.log_to_csv(query, answer)
+            st.chat_message("assistant").write(answer)
 
 if __name__ == "__main__":
     run_app()
